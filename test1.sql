b0a141deca33e5c82cfe87770cdee28a83e9da9a
Danh Nguyen <65102457+danny-cpp@users.noreply.github.com>
2020-08-09 23:40:43 -0400

    nested

--- test1.sql
+++ test1.sql
@@ -8,4 +8,21 @@
 --         (select DEPT_ID_DEP from DEPARTMENTS
 --             where DEP_NAME in ('Software Group', 'Design Team'));
 
-select * from DEPARTMENTS, EMPLOYEES;
+select * from EMPLOYEES, DEPARTMENTS
+    where DEPT_ID_DEP = DEP_ID;
+
+select min(SALARY) from EMPLOYEES;
+
+select distinct(FIRST_NAME) from INSTRUCTOR;
+
+select F_NAME from EMPLOYEES
+    where SALARY =
+          (select max(SALARY) from EMPLOYEES);
+
+select * from EMPLOYEES
+    where DEP_ID =
+          (select max(DEP_ID) from EMPLOYEES);
+
+select F_NAME, DEP_NAME from EMPLOYEES, DEPARTMENTS
+    where DEP_ID = DEPT_ID_DEP ;
+
