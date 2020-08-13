915104622d12f3efb60f2459dbcd49123b72b198
Danh Nguyen <65102457+danny-cpp@users.noreply.github.com>
2020-08-12 23:07:01 -0400

    More Data with Python

---
+++ Python_for_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+# Import clean data
+path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/module_5_auto.csv'
+df = pd.read_csv(path)
+
+df = df._get_numeric_data()
+df.head()
+
