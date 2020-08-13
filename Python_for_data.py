b8999c15a55bd3db8d1bc41a195b09cd454a914a
Danh Nguyen <65102457+danny-cpp@users.noreply.github.com>
2020-08-12 23:18:18 -0400

    Linear Regression

--- Python_for_data.py
+++ Python_for_data.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
-# Import clean data
-path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/module_5_auto.csv'
+path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv'
 df = pd.read_csv(path)
+print(df.head())
 
-df = df._get_numeric_data()
-df.head()
+lm = LinearRegression()
+X = df[['highway-mpg']]
+Y = df['price']
+
+lm.fit(X, Y)
+
 
